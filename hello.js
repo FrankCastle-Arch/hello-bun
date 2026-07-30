4439328ea8e59b53e955bb59855b0cfe95a12c02
FrankCastle-Arch <frankcastle.arch@gmail.com>
2026-07-29 22:29:14 -0400

    print the date

--- hello.js
+++ hello.js
@@ -2,3 +2,4 @@ let course = "Level 2";
 let message = "Hello from " + course + "!";
 console.log(message);
 console.log("This Javascript runs on MY machine.");
+console.log("Today is July 29, 2026")
